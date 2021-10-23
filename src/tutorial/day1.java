0c068c961bb453ac9b54e8dd7aa580db051818a7
looser-xxx <itismaurya4769@gmail.com>
2021-10-23 17:06:31 +0530

    first commit

---
+++ src/tutorial/day1.java
@@ -0,0 +1,17 @@
+//Data structures: the way in which memory is stored.
+//
+//Algorithms: the way of doing some computations.
+//
+//Companies look for coders with a strong grasp on DS Algo.
+
+
+
+package tutorial;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("first java program of this tutorial");
+    }
+}
