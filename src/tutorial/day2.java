319916e87f0eda06f1c9318bb8f91532a9b723f1
looser-xxx <itismaurya4769@gmail.com>
2021-10-23 17:14:27 +0530

    day 2 complete

---
+++ src/tutorial/day2.java
@@ -0,0 +1,18 @@
+
+//already had installed java and IDE
+
+
+
+
+
+
+
+package tutorial;
+
+public class day2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("day2");
+    }
+}
