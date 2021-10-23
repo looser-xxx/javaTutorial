f1238492aafa8605e71487e7d60806ce745b5426
looser-xxx <itismaurya4769@gmail.com>
2021-10-23 17:30:47 +0530

    day 3 complete

---
+++ src/tutorial/day3.java
@@ -0,0 +1,32 @@
+package tutorial;
+
+public class day3
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Day 3");
+
+
+//        1 bite = 8 bits = 2^8
+        int age = 10;
+        System.out.println("age : "+age);
+        float rateOfInterest = 2.3f;
+        System.out.println("rate : "+rateOfInterest);
+        double rate=3.1428571;
+        System.out.println(rate);
+        boolean isThisSeriesCool=true;
+        System.out.println("It is "+isThisSeriesCool+" that this series is cool");
+        char myCharacter ='@';
+        System.out.println("my character is : "+myCharacter);
+
+
+
+
+
+
+
+
+
+
+    }
+}
