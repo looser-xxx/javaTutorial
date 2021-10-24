4d56115422fc5c4562cf86c013732204a723fd54
looser-xxx <itismaurya4769@gmail.com>
2021-10-24 08:43:54 +0530

    day 10 done

---
+++ src/tutorial/day10.java
@@ -0,0 +1,159 @@
+package tutorial;
+
+public class day10
+{
+    public static void main(String[] args)
+    {
+        System.out.println("day 10");
+        System.out.println("loops");
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println();
+        System.out.println("for loop");
+//        for(initilisation ; condition ; reinitilisation)
+//        {
+//
+//        }
+
+
+        System.out.println();
+        for(int i=0;i<100; i++)
+        {
+            System.out.println("Hello World "+i);
+        }
+
+        System.out.println();
+        for(int i=0;i<=100;i+=2)
+        {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        int sum=0;
+        int n=5;
+        for(int i=1;i<=n;i++)
+            sum+=i;
+        System.out.println(sum);
+
+
+        System.out.println();
+        for(int i=100;i>=1;i--)
+            System.out.println(i);
+
+
+        System.out.println();
+        int tableOf=4;
+        for(int i=1;i<11;i++)
+            System.out.println(tableOf+" X "+i+" = "+(tableOf*i));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
